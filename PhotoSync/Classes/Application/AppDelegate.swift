a792b2f8d7a8c5e9295ffdc3137a136a5d5358ab
Julian Grosshauser <julian@juliangrosshauser.com>
2015-03-02 23:30:55 +0100

    Add Xcode project plus contents

---
+++ PhotoSync/Classes/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  PhotoSync
+//
+//  Created by Julian Grosshauser on 28/02/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ PhotoSyncTests/Classes/PhotoSyncTests.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoSyncTests.swift
+//  PhotoSyncTests
+//
+//  Created by Julian Grosshauser on 28/02/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PhotoSyncTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssert(true, "Pass")
+    }
+}
